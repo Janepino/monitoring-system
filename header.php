a24c5d7b0018ab29a92a87e5dbfdab964e58ef33
Janepino <christyjanevosotros.pino@my.smciligan.edu.ph>
2026-01-29 13:00:12 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<div class="header">
+    <div class="logo">DMA AUTO GARAGE</div>
+    <nav class="nav">
+        <a href="dashboard.php">Dashboard</a>
+        <a href="customers.php">Customers</a>
+        <a href="vehicles.php">Vehicles</a>
+        <a href="mechanics.php">Mechanics</a>
+        <a href="spare_parts.php">Spare Parts</a>
+        <a href="reports.php">Reports</a>
+        <a href="logout.php" class="logout-btn">Logout</a>
+    </nav>
+</div>
